3ceac6e1cc03e752cdcfa0d31ef63c571a89ae90
nicolewallace09 <nicole.elisaw@gmail.com>
2020-08-12 18:22:28 -0700

    playing around with body of homepage

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -5,9 +5,17 @@ const Homepage = () => {
         <>
         <div class="jumbotron-fluid"></div>
 
-        <div class="row stats-container">
-            <div class="col-6">
+        <div class="row stats-container text-center">
+            <div class="col-6 card">
                 <h1 class="section">Global Placeholder</h1>
+                <div class="row global-container">
+                    <div class="col-12">
+                        <p class="badge badge-primary">Confirmed:</p> Example# <p class="badge badge-warning">Active:</p> Example#<br></br>
+                    </div>
+                    <div class="col-12">
+                        <p class="badge badge-success">Recovered:</p> Example# <p class="badge badge-danger">Deaths:</p> Example#
+                    </div>
+                </div>
             </div>
 
             <div class="col-6">
