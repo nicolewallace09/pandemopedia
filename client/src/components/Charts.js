17a366ab2932ff80613518e553f0d8ecceb432ea
JoshPrice315 <61852564+JoshPrice315@users.noreply.github.com>
2020-08-16 14:55:19 -0700

    changes made with Aylan

--- client/src/components/Charts.js
+++ client/src/components/Charts.js
@@ -1,14 +1,13 @@
-import React, { PureComponent } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
 } from 'recharts';
 import { covidDataCurrent } from '../utils/API'
 
 
-
 const data = [
   {
-    name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
+    name: 'Page A', uv: 4000,
   },
   {
     name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
@@ -30,29 +29,31 @@ const data = [
   },
 ];
 
-export default class Example extends PureComponent {
-  static jsfiddleUrl = 'https://jsfiddle.net/alidingling/30763kr7/';
+export default function () {
+  const jsfiddleUrl = 'https://jsfiddle.net/alidingling/30763kr7/';
 
-   
+  const [apiData, setApiData] = useState([]);
+  useEffect(() => {
 
-  render() {
-    return (
-      <BarChart
-        width={500}
-        height={300}
-        data={data}
-        margin={{
-          top: 5, right: 30, left: 20, bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Bar dataKey="pv" fill="#8884d8" />
-        <Bar dataKey="uv" fill="#82ca9d" />
-      </BarChart>
-    );
-  }
+  });
+
+  return (
+    <BarChart
+      width={500}
+      height={300}
+      data={apiData}
+      margin={{
+        top: 5, right: 30, left: 20, bottom: 5,
+      }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Bar dataKey="pv" fill="#8884d8" />
+      <Bar dataKey="uv" fill="#82ca9d" />
+    </BarChart>
+  );
 }
+
