7fe53511e762b5b4d1fc317f0c45beaf802fd525
nicolewallace09 <nicole.elisaw@gmail.com>
2020-08-30 17:58:37 -0700

    Updates

---
+++ precache-manifest.b2cd07d78c6adef62079615d258f4e2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6d62f8c91090f33468b1917c443feb9",
+    "url": "/pandemopedia/index.html"
+  },
+  {
+    "revision": "d3602da9924d2e0a9b30",
+    "url": "/pandemopedia/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "06f3c083dcbf640883de",
+    "url": "/pandemopedia/static/css/main.62dc221e.chunk.css"
+  },
+  {
+    "revision": "d3602da9924d2e0a9b30",
+    "url": "/pandemopedia/static/js/2.b34ced8f.chunk.js"
+  },
+  {
+    "revision": "9480074262b9fa511b35730b8d669c53",
+    "url": "/pandemopedia/static/js/2.b34ced8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06f3c083dcbf640883de",
+    "url": "/pandemopedia/static/js/main.5fece0d1.chunk.js"
+  },
+  {
+    "revision": "876d19486ea26a60d0e9",
+    "url": "/pandemopedia/static/js/runtime-main.862ec522.js"
+  }
+]);
