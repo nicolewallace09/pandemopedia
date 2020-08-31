da7c3f1cedc158fea7de38d1287a60c26495e3f0
nicolewallace09 <nicole.elisaw@gmail.com>
2020-08-30 18:53:21 -0700

    trying new changes

--- server/server.js
+++ server/server.js
@@ -24,11 +24,11 @@ app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/public')));
+  app.use(express.static(path.join(__dirname, '..client/build')));
 }
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/public/index.html'));
+  res.sendFile(path.join(__dirname, '..client/build/index.html'));
 })
 
 db.once('open', () => {
